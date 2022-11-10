a9d1c75058d777e835f4bfd2b48dd4feb53badb3
tracer-ics2020 <tracer@njuics.org>
2022-11-10 16:41:30 +0800

    >  compile 209074279    Static hostname: Night          Icon name: computer-laptop            Chassis: laptop         Machine ID: fd3a082b1625491a918b1dc0cad12057            Boot ID: 1aa85cdfa0b64502b5d0567711656e23   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-49-generic       Architecture: x86-64  16:41:30 up 17 min,  4 users,  load average: 0.54, 0.61, 0.47

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -24,7 +24,7 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
-  {"\(",'('},
+  {"/\(",'('},
   {"/[0-9]",TK_DIGIT},     //digits
   {"\\-",'-'},
   {"\\*",'*'},
